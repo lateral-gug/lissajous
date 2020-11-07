cf06ad9b0bdf97e7d802559f46b3317e1e791279
Guglielmo Pellitteri <67461869+lateral-gug@users.noreply.github.com>
2020-11-07 20:20:35 +0100

    dampening parameters update

--- lissajous.py
+++ lissajous.py
@@ -5,19 +5,24 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 
 #x and y motion parameters
-px = [1,3,0.5]
-py = [1,2,0]
+px = [1,5,0]
+py = [1,3,np.pi/2]
+
+#dampening coefficients
+xdamp = 0.01
+ydamp = 0.01
 
 #animation parameters
-time = 100
+time = 2*np.pi*7
 frames = 10000
 interval = 5
+opacity = 0.7
 
 def fx(t):
-    return px[0]*np.cos(px[1]*t+px[2])
+    return px[0]*np.exp(-xdamp*t)*np.cos(px[1]*t+px[2])
 
 def fy(t):
-    return py[0]*np.cos(py[1]*t+py[2])
+    return py[0]*np.exp(-ydamp*t)*np.cos(py[1]*t+py[2])
 
 xx = yy = np.array([])
 
@@ -35,11 +40,11 @@ def animate(i):
 
 fig = plt.figure()
 
-plt.plot(xx,yy,'r-',alpha=0.2)
+plt.plot(xx,yy,'r-',alpha=opacity)
 
 redDot, = plt.plot(xx,yy,'k.',markersize=15,zorder=3)
 
-myAnimation1 = anim.FuncAnimation(fig, animate, frames=frm, interval=inter, blit=True, repeat=False)
+myAnimation1 = anim.FuncAnimation(fig, animate, frames=frm, interval=inter, blit=True, repeat=True)
 
 plt.grid(linestyle=':')
 plt.show()
